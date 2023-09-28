5060a5b4bc54fb9b606e245fa2876b736f19aed7
QCaoNo1 <145078207+QCaoNo1@users.noreply.github.com>
2023-09-28 13:46:54 +0700

    Create trangchu.js

---
+++ front-end/trangchu.js
@@ -0,0 +1,231 @@
+<div>
+      <div class="container">
+        <div class="menu">
+          <div class="header_menu">
+
+            <span class="info-left">
+              <span>
+                <span class="cach1"></span>
+                <a class="left">Blog</a>
+                <span class="cach1"></span>
+              </span>
+              <span>
+                <a href="" class="left">Mua bán</a>
+                <span class="cach1"></span>
+              </span>
+              <span>
+                <a href="" class="left">Đánh giá</a>
+                <span class="cach1"></span>
+              </span>
+              <span>
+                <a href="" class="left">Liên hệ</a>
+              </span>
+            </span>
+          </div>
+
+
+          <div class="container1">
+            <div class="box box1">
+              <div class="logo-f">
+                <a href='/'>
+                  <img src={logo} alt='' />
+                </a>
+              </div>
+            </div>
+            <div class="box box2">
+              <div class="dropdown">
+                <button class="dropbtn"><i class="fa-solid fa-bars"></i> Danh mục
+                </button>
+
+
+                <div class="dropdown-content">
+                  <a href="#">Chó</a>
+                  <a href="#">Mèo</a>
+                  <a href="#">Sản phẩm cho chó mèo</a>
+
+                </div>
+
+              </div>
+              <i class=""></i>
+            </div>
+            <div class="box box3">
+              <div class="input-container">
+                <input type="text" class="input" placeholder="Tìm kiếm..." />
+                <button class="search-button" ><i class="fa fa-search" aria-hidden="true"></i> Tìm kiếm </button>
+              </div>
+              <div class="quanly">
+                <a href='' class="dangnhap"><i class="fa-solid fa-file"></i> Quản lý tin</a>
+              </div>
+            </div>
+            <div class="box box4">
+              <div class="login-container">
+                <div class="login-text">
+                  <a href="#" class="dangnhap" >Đăng nhập/Đăng ký </a>
+                </div>
+                <div class="post-button">
+                  <button class="button"> <i class="fa-solid fa-paper-plane"></i> Đăng bài</button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="body-container">
+        <div class="container2">
+          <div class="box-body1 ">
+            <div class="top-body1">
+
+
+              <div class="slider">
+
+                <div class="slider-content">
+                  <div class="slider-content-left">
+                    <div class="slider-content-left-top">
+                      <a href=""><img src={sliders} alt="" /></a>
+
+                      <div class="slider-content-left-top-btn">
+                        <i class="fas fa-chevron-left"></i>
+                        <i class="fas fa-chevron-right"></i>
+                      </div>
+
+                    </div>
+
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="below-body1">
+              <div class="top-bl-bd1">
+                <h3>Khám phá danh mục</h3>
+              </div>
+              <div class="category-container">
+                <div class="category">
+                  <div> <img src="https://i.pinimg.com/564x/cf/e6/6c/cfe66c20d118d57ff423014e06e224b1.jpg" /></div>
+                  <div> <a href="#">Chó</a></div>
+
+                </div>
+                <div class="category">
+                  <div><img src="https://i.pinimg.com/236x/7c/ea/c6/7ceac6be8e135bdfa43ff4880b3e9a02.jpg" /></div>
+                  <div> <a href="">Mèo</a>
+                  </div>
+
+                </div>
+                <div class="category">
+                  <div>
+                    <img src="https://i.pinimg.com/236x/ca/c0/0c/cac00c5cb2431b0117202b35c99ff1ef.jpg"
+                    />
+                  </div>
+                  <div class="title">
+                    <a href="">Sản phâm cho chó mèo</a>
+                  </div>
+                </div>
+                <div class="category">
+                  <div>
+                    <img src="https://zoipet.com/wp-content/uploads/2019/04/tam-cho-dung-cach.jpg" />
+                  </div>
+                  <div>
+                    <a href="">Bảng Servive</a>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+
+          <div class="box-body1">
+            <div class="top-body2">
+              <div><h3>Tin đăng chó mèo</h3></div>
+              <div><a href="">xem thêm</a></div>
+            </div>
+            <div class="listing-box">
+              <a href="link1.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/564x/69/a8/2a/69a82a563703fc99d79af166cadebdf6.jpg" alt="Hình ảnh sản phẩm 1" />
+                </div>
+                <h3>Corgi</h3>
+                <p>TP.Hồ Chí Minh</p>
+                <p>xxx đồng</p>
+
+              </a>
+              <a href="link2.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/da/a6/d0/daa6d0c2e1f940fc6d2fd4307373d85d.jpg" alt="Hình ảnh sản phẩm 2" />
+                </div>
+                <h3>Tên sản phẩm 2</h3>
+                <p>Địa chỉ sản phẩm 2</p>
+                <p>Giá sản phẩm 2</p>
+              </a>
+              <a href="link3.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/564x/f6/bd/ea/f6bdea6dc8146897d445254f8833de3f.jpg" alt="Hình ảnh sản phẩm 3" />
+                </div>
+                <h3>Tên sản phẩm 3</h3>
+                <p>Địa chỉ sản phẩm 3</p>
+                <p>Giá sản phẩm 3</p>
+              </a>
+              <a href="link4.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/bf/a9/b1/bfa9b174f912ef465ceb29e6dd69c8a6.jpg" alt="Hình ảnh sản phẩm 4" />
+                </div>
+                <h3>Tên sản phẩm 4</h3>
+                <p>Địa chỉ sản phẩm 4</p>
+                <p>Giá sản phẩm 4</p>
+              </a>
+            </div>
+
+          </div>
+          <div class="box-body1">
+            <div class="top-body2">
+              <div><h3>Tin đăng sản phẩm mới</h3></div>
+              <div><a href="">xem thêm</a></div>
+            </div>
+            <div class="listing-box">
+              <a href="link1.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/28/f0/88/28f08803811ca9742f3c2163fbf404e9.jpg" alt="Hình ảnh sản phẩm 1" />
+                </div>
+                <h3>Tên sản phẩm 1</h3>
+                <p>Địa chỉ sản phẩm 1</p>
+                <p>Giá sản phẩm 1</p>
+              </a>
+              <a href="link2.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/f8/dd/e3/f8dde38d3e77fb686a9dd09f35065ddd.jpg" alt="Hình ảnh sản phẩm 2" />
+                </div>
+                <h3>Tên sản phẩm 2</h3>
+                <p>Địa chỉ sản phẩm 2</p>
+                <p>Giá sản phẩm 2</p>
+              </a>
+              <a href="link3.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/1b/66/17/1b6617cf347d13acab4a1815223f1225.jpg" alt="Hình ảnh sản phẩm 3" />
+                </div>
+                <h3>Tên sản phẩm 3</h3>
+                <p>Địa chỉ sản phẩm 3</p>
+                <p>Giá sản phẩm 3</p>
+              </a>
+              <a href="link4.html" class="listing">
+                <div class="post">
+                  <img src="https://i.pinimg.com/236x/ca/c0/0c/cac00c5cb2431b0117202b35c99ff1ef.jpg" alt="Hình ảnh sản phẩm 4" />
+                </div>
+                <h3>Tên sản phẩm 4</h3>
+                <p>Địa chỉ sản phẩm 4</p>
+                <p>Giá sản phẩm 4</p>
+              </a>
+            </div>
+
+          </div>
+
+
+        </div>
+
+      </div>
+
+    </div>
+
