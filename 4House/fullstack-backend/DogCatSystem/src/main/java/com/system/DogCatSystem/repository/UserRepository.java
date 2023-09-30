f79efa72309168c6b604cd5c8f647e0ae2b4becf
sonnh1106 <nguyenson20zzzz@gmail.com>
2023-09-30 11:06:16 +0700

    GitPro

--- 4House/fullstack-backend/DogCatSystem/src/main/java/com/system/DogCatSystem/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.DogCatSystem.repository;
-
-import com.system.DogCatSystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
