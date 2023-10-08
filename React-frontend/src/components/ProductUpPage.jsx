5ad0e8423f62d9f59d6c12fd66ce3893d808af8e
sonnh1106 <nguyenson20zzzz@gmail.com>
2023-10-08 12:00:07 +0700

    Updated

---
+++ React-frontend/src/components/ProductUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './layout/Navbar';
+import ProductUpForm from './layout/ProductUpForm';
+export default function ProductPostPage() {
+  return (
+    <div>
+        <Navbar/>
+        <ProductUpForm/>
+    </div>
+  )
+}
