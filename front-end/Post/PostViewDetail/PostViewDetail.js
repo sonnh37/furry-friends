6ef25a8c7c3ca6554d75f015d0bfcf797ee0e163
QCaoNo1 <145078207+QCaoNo1@users.noreply.github.com>
2023-10-09 15:22:51 +0700

    Create PostViewDetail.js

---
+++ front-end/Post/PostViewDetail/PostViewDetail.js
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Post</title>
+    <link rel="stylesheet" href="stylePostDetail.css">
+    <script src="https://kit.fontawesome.com/54f0cb7e4a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+
+<div class="body">
+  <a href="">
+  <div class="body-blog1">
+    <div class="body-blog1-detail">
+      <div class="name">
+        <div class=""><i><i class="fa fa-user" aria-hidden="true"></i></i> Minh Khôi</div>
+        <div class="time">9:35 4/10/2023</div>
+      </div>
+      <div class="body-blog1-title">
+        <p class="body-blog1-title-detail"> Tựa đề </p>
+      </div>
+      <div class="body-blog1-describe">
+        <p class="body-blog1-describe-detail">Đây là mô tả của blog, vd như
+            chú chó của tôi rất vui khi được chụp hình, nó thường cười nhe răng
+            khi thấy máy ảnh quay về hướng của nó, tôi cũng thấy đó là niềm vui của tôi
+            khi nhìn nó vui vẻ.
+        </p>
+    </div>
+    </div>
+    <div class="body-blog1-img">
+      <img class="body-blog1-img-detail" src="smileofdog.jpg" alt="The dog is smiling">
+    </div>
+  
+    <div class="body-blog1-button">
+        <button class="body-blog1-button-likeButton" aria-hidden="true">
+          100 <i class="far fa-thumbs-up fa-1x"></i>
+        </button>
+        <button class="body-blog1-button-commentButton" aria-hidden="true">
+         100 <i class="far fa-comment fa-1x"></i>
+        </button>
+    </div>
+
+    <div class="body-blog-1-otherComments">
+      <div class="body-blog1-ortherComments-userName">
+          <h4 class="body-blog1-ortherComments-userName-detail">Hoàng Sơn</h4>
+      </div>
+      <div class="body-blog1-ortherComments-theComment">
+          <p class="body-blog1-ortherComments-theComment-detail">Comment số 1</p>
+      </div>
+      <hr>
+      <div class="body-blog1-ortherComments-userName">
+          <h4 class="body-blog1-ortherComments-userName-detail">Minh Quân</h4>
+      </div>
+      <div class="body-blog1-ortherComments-theComment">
+          <p class="body-blog1-ortherComments-theComment-detail">Comment số 2</p>
+      </div>
+      <hr>
+      <div class="body-blog1-ortherComments-userName">
+          <h4 class="body-blog1-ortherComments-userName-detail">Minh Vương</h4>
+      </div>
+      <div class="body-blog1-ortherComments-theComment">
+          <p class="body-blog1-ortherComments-theComment-detail">Comment số 3</p>
+      </div>
+  </div>
+  </div>
+</a>
+</div>
+</div>
