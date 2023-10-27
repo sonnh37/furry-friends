64f634ff2e769774fa24d13686e2b4006ec8a081
sonnh1106 <nguyenson20zzzz@gmail.com>
2023-10-27 22:20:33 +0700

    Add product img

--- back-end/backend/src/main/java/com/system/backend/Dto/Product/ProductResponse.java
+++ back-end/backend/src/main/java/com/system/backend/Dto/Product/ProductResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Lob;
 import java.util.List;
 
 @Data
@@ -16,6 +17,7 @@ public class ProductResponse {
     private Integer user_id;
     private String product_name;
     private Float price;
+    @Lob
     private List<String> img_productList;
     private String description;
     private String date;
